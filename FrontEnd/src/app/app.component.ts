36fad8d40736d472b2c5d36dc5602ff1d5445aef
ramonb1996 <ramon.brokking@gmail.com>
2023-03-20 14:22:44 +0100

    Added bootstrap and dummy table to front-end

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Course } from './models/course';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  title = 'InfoSupport Cursussen';
+
+  courses: Course[]= [
+    { id: 1, title: "Programming with C#", startDate: "2020-08-31", duration: 5},
+    { id: 2, title: "ASP.NET", startDate: "2020-08-31", duration: 2}
+  ];
 }
