b1803a6bb136da199855010c1e80992b618e3ddb
ramonb1996 <ramon.brokking@gmail.com>
2023-03-21 16:06:05 +0100

    Added UnitTest project for WebApi and wrote Unit tests for cases that should fail in FileParser

---
+++ WebApi/WebApi.Tests/FileParserTests.cs
@@ -0,0 +1,56 @@
+﻿namespace WebApi.Tests;
+
+public class FileParserTests
+{
+    private IFormFile CreateMockFile(string fileContent)
+    {
+        var content = fileContent;
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(content);
+        writer.Flush();
+        stream.Position = 0;
+
+        return new FormFile(stream, 0, stream.Length, string.Empty, "file.txt");
+    }
+
+    [Fact]
+    public async Task ParseFileToCoursesAsync_WrongOrder_Throws_FileFormatException()
+    {
+        IFormFile file = CreateMockFile("Titel: C# Programmeren\nDuur: 5 dagen\nCursuscode: CNETIN\nStartdatum: 8/10/2018\n\n");
+
+        await Assert.ThrowsAsync<FileFormatException>(() => FileParser.ParseFileToCoursesAsync(file));
+    }
+
+    [Fact]
+    public async Task ParseFileToCoursesAsync_NotEnoughData_Throws_FileFormatException()
+    {
+        IFormFile file = CreateMockFile("Titel: C# Programmeren\nDuur: 5 dagen\nCursuscode: CNETIN\nStartdatum: 8/10/2018\n\n");
+
+        await Assert.ThrowsAsync<FileFormatException>(() => FileParser.ParseFileToCoursesAsync(file));
+    }
+
+    [Fact]
+    public async Task ParseFileToCoursesAsync_WrongStartDateFormat_Throws_FormatException()
+    {
+        IFormFile file = CreateMockFile("Titel: C# Programmeren\nCursuscode: CNETIN\nDuur: 5 dagen\nStartdatum: 8-10-2018\n\n");
+
+        await Assert.ThrowsAsync<FormatException>(() => FileParser.ParseFileToCoursesAsync(file));
+    }
+
+    [Fact]
+    public async Task ParseFileToCoursesAsync_WrongDurationFormat_Throws_FileFormatException()
+    {
+        IFormFile file = CreateMockFile("Titel: C# Programmeren\nCursuscode: CNETIN\nDuur: 5\nStartdatum: 8/10/2018\n\n");
+
+        await Assert.ThrowsAsync<FileFormatException>(() => FileParser.ParseFileToCoursesAsync(file));
+    }
+
+    [Fact]
+    public async Task ParseFileToCoursesAsync_NoTrailingEmptyLine_BetweenEntities_Throws_FileFormatException()
+    {
+        IFormFile file = CreateMockFile("Titel: C# Programmeren\nCursuscode: CNETIN\nDuur: 5 dagen\nStartdatum: 8/10/2018\nTitel: Java Persistence API\nCursuscode: JPA\nDuur: 2 dagen\nStartdatum: 10/10/2018\n\n");
+
+        await Assert.ThrowsAsync<FileFormatException>(() => FileParser.ParseFileToCoursesAsync(file));
+    }
+}
---
+++ WebApi/WebApi.Tests/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using Xunit;
+global using Microsoft.AspNetCore.Http;
+global using WebApi.Exceptions;
+global using WebApi.Helpers;
